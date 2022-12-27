0dbd1bc95c81739f802059f92416289af40122c7
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-12-27 15:28:31 +0100

    Added Exercise_9 and modified Hello and Message components

---
+++ Exercise_9/components_07/src/Hello.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Message } from "./Message";
+
+export class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World! 👋🏻</h1>
+        <Message />
+      </div>
+    );
+  }
+}
