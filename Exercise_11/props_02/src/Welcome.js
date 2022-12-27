46756a3b49bdeb84569345f96ab8ac06facd56d0
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-12-27 16:04:15 +0100

    Added Exercise_11 and modified Welcome component setting default props value

---
+++ Exercise_11/props_02/src/Welcome.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Welcome extends React.Component {
+  render() {
+    return <p> Welcome, {this.props.name}! 😃</p>;
+  }
+}
+
+Welcome.defaultProps = {
+  name: 'Bianca',
+}
