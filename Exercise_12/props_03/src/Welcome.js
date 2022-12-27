93c10bc90348938ecc6ff59cbb45f69589e498d9
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-12-27 16:13:52 +0100

    Added Exercise_12 and modified Welcome component props

---
+++ Exercise_12/props_03/src/Welcome.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <p> Welcome, {this.props.name}! 😃</p>
+        <p> Your Age is: {this.props.age} 👍🏻</p>
+        </div>
+    );
+  }
+}
+
+Welcome.defaultProps = {
+  name: 'Bianca',
+  age: 30,
+}
