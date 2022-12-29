9d4e3f780c51fe29c5462f3c0b42ab5933585946
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-12-29 08:39:33 +0100

    Initialized Exercise_16 and changed Age component to be conditionally rendered in Welcome component

--- Exercise_16/conditional-rendering_02/src/Age.js
+++ Exercise_16/conditional-rendering_02/src/Age.js
@@ -3,7 +3,7 @@ import React from "react";
 export class Age extends React.Component {
   render() {
     if (this.props.age) {
-      return <p> Your Age is: {this.props.age}</p>;
+      return <p> Your Age is: {this.props.age} 💪🏻</p>;
     }
   }
 }
