d4fce6e095e08134cbb1f975b3bda31bce6e05aa
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2022-12-29 21:57:41 +0100

    modified/fixed Welcome component to be conditionally rendered

--- Exercise_17/conditional-rendering_03/src/Age.js
+++ Exercise_17/conditional-rendering_03/src/Age.js
@@ -2,8 +2,6 @@ import React from "react";
 
 export class Age extends React.Component {
   render() {
-    if (this.props.age > 18 && this.props.age < 65) {
-      return <p> Your Age is: {this.props.age} 💪🏻</p>;
-    }
+    return <p> Your Age is: {this.props.age} 💪🏻</p>;
   }
 }
--- Exercise_17/conditional-rendering_03/src/Welcome.js
+++ Exercise_17/conditional-rendering_03/src/Welcome.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { Age } from "./Age";
 
+const ageDetails = {
+  _age: 25,
+}
+
 export class Welcome extends React.Component {
   render() {
       return (
         <div>
           <p>Welcome, {this.props.name}! 😃</p>
-          <Age age={50} />
+          {ageDetails._age > 18 && ageDetails._age < 65 && <Age age={ageDetails._age} />}
         </div>
       );
   }
