d32fb0c1456c0f1f539a09b2137ca1d02f56e9ad
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-04 11:10:34 +0100

    Inizialized Exercise_28

---
+++ Exercise_28/forms_03/src/Login.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export class Login extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    username: "",
+    password: "",
+    remember: false,
+    buttonDisabled: true,
+  };
+
+  this.onLogin = () => {
+    console.log(this.state);
+  };
+  }
+
+  handlerInputChanged = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    const checkedValue = e.target.checked;
+
+    console.log(e);
+    this.setState({
+      [name]: value,
+      [checkedValue]: true,
+      buttonDisabled: this.state.username && this.state.password ? false : true,
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          name="username"
+          type="input"
+          value={this.state.username}
+          onChange={this.handlerInputChanged}
+        />
+        <input
+          name="password"
+          type="password"
+          value={this.state.password}
+          onChange={this.handlerInputChanged}
+        />
+        <input
+          name="remember"
+          type="checkbox"
+          value={this.state.remember}
+          onChange={this.handlerInputChanged}
+        />
+        <button name="button" onClick={this.onLogin} disabled={this.state.buttonDisabled}> Login </button>
+      </div>
+    );
+  }
+}
