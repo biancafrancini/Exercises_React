66233dea978e9b0ffb1c2dd2047e637a1dc0accf
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-04 09:09:06 +0100

    added adjustments and modified explanation about constructor

--- Exercise_23/component-lifecycle_01/src/Counter.js
+++ Exercise_23/component-lifecycle_01/src/Counter.js
@@ -2,20 +2,21 @@ import React from "react";
 import { CounterDisplay } from "./CounterDisplay";
 
 export class Counter extends React.Component {
-  state = {
-    count: this.props.initialValue,
-    increment: this.props.incrementValue,
-    interval: this.props.intervalTime,
-  };
-
-  //In this case, the constructor is not required (but there's no harm in adding it).
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: this.props.initialValue,
+      //increment: this.props.incrementValue,
+      //interval: this.props.intervalTime,
+    };
+  }
 
   componentDidMount() {
     setInterval(() => {
       this.setState({
-        count: this.state.count + this.state.increment,
+        count: this.state.count + this.props.incrementValue,
       });
-    }, this.state.interval);
+    }, this.props.intervalTime);
   }
 
   render() {
@@ -26,3 +27,14 @@ export class Counter extends React.Component {
     );
   }
 }
+
+
+/* ENGLISH:
+In this case, we don't need to initialize the interval value inside the constructor.
+However, the initialization of a starting value for the counter is still required to be able to increment
+the counter within the selected interval (and then update the state) */
+
+/* ITALIANO:
+In questo caso, non abbiamo bisogno di inizializzare l'intervallo all'interno del constructor.
+Tuttavia, l'inizializzazione di un valore iniziale, all'interno di un constructor, o state, è ancora necessaria per 
+poter incrementare il counter all'interno dell'intervallo di tempo indicato (e di conseguenza, aggiornare lo state). */
