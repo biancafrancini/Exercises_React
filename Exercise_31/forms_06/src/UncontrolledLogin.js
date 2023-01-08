99061e597cf53632abec548f61a1b5a53171d47a
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-08 09:36:33 +0100

    Initialized Exercise_31 and added autoFocus attribute to the input username field

---
+++ Exercise_31/forms_06/src/UncontrolledLogin.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export class UncontrolledLogin extends React.Component {
+
+  handleForm = (e) => {
+    e.preventDefault();
+    const username = e.target.elements.username.value;
+    const password = e.target.elements.password.value;
+    const remember = e.target.elements.remember.checked;
+    const button = username && password ? e.target[3].disabled : !e.target[3].disabled;
+
+    //console.log(e);
+    console.log({ username, password, remember, button});
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleForm}>
+          <input name="username" type="input" autoFocus/>
+          <input name="password" type="password" />
+          <input name="remember" type="checkbox" />
+          <button type="submit">
+            Login
+          </button>
+          <br />
+          <br />
+          <button type="reset"> Reset </button>
+        </form>
+      </div>
+    );
+  }
+}
