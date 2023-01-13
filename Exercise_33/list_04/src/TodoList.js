91afa2a81cd4f1ba8fcbdb751af43b64fb3f2a50
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-13 15:43:45 +0000

    state adjustments

--- Exercise_33/list_04/src/TodoList.js
+++ Exercise_33/list_04/src/TodoList.js
@@ -1,43 +1,39 @@
 import React from "react";
 
 export class TodoList extends React.Component {
-  
-    state = {
-      items: [
-        "Have breakfast",
-        "Study",
-        "Have lunch",
-        "Do workouts",
-        "Go to the doctor",
-        "Order food for the whole week",
-        "Have dinner",
-      ],
-    };
+  state = {
+    items: [
+      "Have breakfast",
+      "Study",
+      "Have lunch",
+      "Do workouts",
+      "Go to the doctor",
+      "Order food for the whole week",
+      "Have dinner",
+    ],
 
-    inputValueState = "";
+    inputValueState: "",
+  };
 
-  getTheItem = () =>{
+  getTheItem = () => {
     const getItemList = this.state.items.map((statesItem, index) => <li key={index + statesItem}>{statesItem}</li>);
     return getItemList;
-  }
+  };
 
- 
-  handleAddButton = () => { 
+  handleAddButton = () => {
     const inputTag = document.querySelector("#newTodo");
- 
-    if(inputTag.value){
+
+    if (inputTag.value) {
       this.state.items.push(inputTag.value);
-  
+
       this.setState({
         items: this.state.items,
-      })
+      });
 
-    //to clear the input field once the new item has been added to the todo list
-    inputTag.value = this.inputValueState; 
-
-    //console.log(this.state.items); 
-   } 
-  }
+      //to clear the input field once the new item has been added to the todo list
+      inputTag.value = this.state.inputValueState;
+    }
+  };
 
   render() {
     return (
@@ -49,8 +45,11 @@ export class TodoList extends React.Component {
           id="newTodo"
           name="newTodo"
           type="input"
-          placeholder="Add a new item... "/>
-        <button type="submit" onClick={this.handleAddButton}>ADD</button>
+          placeholder="Add a new item..."
+        />
+        <button type="submit" onClick={this.handleAddButton}>
+          ADD
+        </button>
       </div>
     );
   }
