b81a9bd49b286f16ca070946c17678cffa0b3545
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-15 09:58:43 +0000

    few adjustments

--- Exercise_30/forms_05/src/UncontrolledLogin.js
+++ Exercise_30/forms_05/src/UncontrolledLogin.js
@@ -1,26 +1,30 @@
 import React from "react";
 
 export class UncontrolledLogin extends React.Component {
-
   handleForm = (e) => {
     e.preventDefault();
     const username = e.target.elements.username.value;
     const password = e.target.elements.password.value;
     const remember = e.target.elements.remember.checked;
-    const button = username && password ? e.target[3].disabled : !e.target[3].disabled;
+    const loginButton = e.target.elements.loginButton.disabled;
 
     //console.log(e);
-    console.log({ username, password, remember, button});
+    console.log({
+      username,
+      password,
+      remember,
+      loginButton: username && password ? loginButton === false : loginButton === true, 
+    });
   };
 
   render() {
     return (
       <div>
         <form onSubmit={this.handleForm}>
-          <input name="username" type="input"/>
+          <input name="username" type="input" />
           <input name="password" type="password" />
           <input name="remember" type="checkbox" />
-          <button type="submit">
+          <button name="loginButton" type="submit">
             Login
           </button>
           <br />
