e121d34a6f17fd628e48ae9cf241a8e4449da995
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-16 15:23:33 +0000

    Initialized Exercise_43 and modified Welcome component to be a function component

---
+++ Exercise_43/function-components_01/src/Welcome.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export function Welcome({ name }) {
+  return (
+    <div className="welcome">
+      <p>Welcome, <strong>{name}</strong>! 😃</p>
+    </div>
+  );
+}
+
+/* Previous solution using class component instead:
+export class Welcome extends React.Component {
+  render() {
+      return (
+        <div className="welcome">
+          <p>Welcome, <strong>{this.props.name}</strong>! 😃</p>
+        </div>
+      );
+  }
+}
+
+Welcome.defaultProps = {
+  name: <strong>You</strong>,
+};
+*/
