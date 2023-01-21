0033bd45af5b5dcfaca1e58451a46830bcb96173
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-21 09:17:58 +0000

    adjustments to CounterComponent structure

--- Exercise_52/custom-hooks_01/src/CounterComponent.js
+++ Exercise_52/custom-hooks_01/src/CounterComponent.js
@@ -1,32 +1,34 @@
 import React, { useState } from "react";
 
-export function CounterComponent({ count = 0 }) {
+function useCounter(count = 0) {
   const [counter, setCounter] = useState(count);
-  const { increment, decrement, reset } = useCounter(null);
 
-  function useCounter(count) {
-    function handleIncrement() {
-      setCounter((current) => current + 1);
-    }
-
-    function handleDecrement() {
-      if (counter <= 0) {
-        setCounter((current) => current = 0);
-      } else{
-        setCounter((current) => current - 1);
-      }   
-    }
-
-    function handleReset() {
-      setCounter((current) => (current = 0));
-    }
-
-    return {
-      increment: handleIncrement,
-      decrement: handleDecrement,
-      reset: handleReset,
-    };
+  function handleIncrement() {
+    setCounter((current) => current + 1);
   }
+
+  function handleDecrement() {
+    if (counter <= 0) {
+      setCounter((current) => (current = 0));
+    } else {
+      setCounter((current) => current - 1);
+    }
+  }
+
+  function handleReset() {
+    setCounter((current) => (current = 0));
+  }
+
+  return {
+    counter: counter,
+    increment: handleIncrement,
+    decrement: handleDecrement,
+    reset: handleReset,
+  };
+}
+
+export function CounterComponent({ count = 0 }) {
+  const { counter, increment, decrement, reset } = useCounter(count);
 
   return (
     <div>
