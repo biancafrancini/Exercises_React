e467943dc0b69c0b103a5bf43652ac811a7ac5fe
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-24 19:51:02 +0000

    Initialized Exercise_57, converted DisplayLanguage into funcional component, added useContext hook to get the same result of Exercise_42: context_02

---
+++ Exercise_57/usecontext/src/DisplayLanguage.js
@@ -0,0 +1,7 @@
+import React, { useContext } from "react";
+import { LanguageContext } from "./LanguageContext";
+
+export function DisplayLanguage() {
+  const language = useContext(LanguageContext);
+  return <h1>{language}</h1>;
+}
