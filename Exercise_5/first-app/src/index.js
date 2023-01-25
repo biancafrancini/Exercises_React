b9a78ac212dbcda218e1601ba9a19e053c6d673e
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-25 14:41:15 +0000

    modified index.js component, removed App.js file

--- Exercise_5/first-app/src/index.js
+++ Exercise_5/first-app/src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Hello } from './Hello';
 
-
-const helloWorld = <Hello />;
+class Hello extends React.Component{
+    render(){
+      return <h1>Hello World! 👋🏻</h1>
+    }
+  };
 
 const $root = ReactDOM.createRoot(document.querySelector("#root"));
-$root.render(helloWorld);
+$root.render(<Hello />);
