2f09a4cfff5498ff087f3b7a5939f02347ac5ca3
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-25 08:48:23 +0000

    modified src folder files

---
+++ Exercise_58/usecallback/src/CounterComponent.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+
+function useCounter(count) {
+  const [counter, setCounter] = useState(count);
+
+  function handleIncrement() {
+    setCounter((current) => current + 1);
+  }
+
+  function handleDecrement() {
+    if (counter <= 0) {
+      setCounter((current) => (current = 0));
+    } else {
+      setCounter((current) => current - 1);
+    }
+  }
+
+  function handleReset() {
+    setCounter((current) => (current = count));
+  }
+
+  return {
+    counter: counter,
+    increment: handleIncrement,
+    decrement: handleDecrement,
+    reset: handleReset,
+  };
+}
+
+export function CounterComponent({ count = 0 }) {
+  const { counter, increment, decrement, reset } = useCounter(count);
+
+  return (
+    <div>
+      <h1>Counter : {counter}</h1>
+      <button onClick={increment}>⇡ Increment counter ⇡ </button>
+      <button onClick={decrement}>⇣ Decrement counter ⇣</button>
+      <button onClick={reset}>Reset counter ↩︎</button>
+    </div>
+  );
+}
