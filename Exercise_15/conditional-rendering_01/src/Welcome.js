8418acfc2d3260398683abff716f057c62ed5f77
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-25 14:44:41 +0000

    modified Welcome component

--- Exercise_15/conditional-rendering_01/src/Welcome.js
+++ Exercise_15/conditional-rendering_01/src/Welcome.js
@@ -1,16 +1,12 @@
 import React from "react";
 import { Age } from "./Age";
 
-const ageProps = {
-  _age: 19,
-}
-
 export class Welcome extends React.Component {
   render() {
     return (
       <div>
         <p> Welcome, {this.props.name}! 😃</p>
-        {ageProps._age > 18 && <Age age={ageProps._age} />}
+        {this.props.age > 18 && <Age age={this.props.age} />}
       </div>
     );
   }
