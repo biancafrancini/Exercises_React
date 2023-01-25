7fd677060f612ad98fe514d491f166bed3293bca
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-25 15:00:19 +0000

    adjustments to Hello component

--- Exercise_5/first-app/src/index.js
+++ Exercise_5/first-app/src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Hello } from "./Hello";
 
-const $root = ReactDOM.createRoot(document.querySelector("#root"));
-$root.render(<Hello />);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<Hello />);
