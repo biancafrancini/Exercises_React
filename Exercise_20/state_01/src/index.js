95e897dba52a7355686e034278fbcb2bb3d25b79
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-28 18:41:24 +0000

    modified README file and adjusted index.js

--- Exercise_20/state_01/src/index.js
+++ Exercise_20/state_01/src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Counter } from "./Counter";
 
-const $root = document.querySelector("#root");
-
-const counterCalculator = <Counter />;
-
-ReactDOM.render(counterCalculator, $root);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<Counter />);
