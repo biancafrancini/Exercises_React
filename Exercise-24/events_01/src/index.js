4ab0a5365c34a8524b4993122fe8646a195183ff
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-28 18:57:29 +0000

    modified README file

--- Exercise-24/events_01/src/index.js
+++ Exercise-24/events_01/src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ClickCounter } from "./ClickCounter";
 
-
-const clickingCounter = <ClickCounter />
-
-const $root = ReactDOM.createRoot(document.querySelector("#root"));
-$root.render(clickingCounter);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<ClickCounter />);
