48fb1d1d28b5da16d81d7c953e63b83d212642db
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-28 17:37:32 +0000

    added a first page and modified link components

--- Exercise_64/react-router_05/src/App.js
+++ Exercise_64/react-router_05/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { ClickCounter } from "./ClickCounter";
 import { Welcome } from "./Welcome";
+import { FirstPage } from "./FirstPage";
 import { ShowGithubUser } from "./ShowGithubUser";
 
 import "./App.css";
@@ -9,17 +10,18 @@ import "./App.css";
 export function App() {
   return (
     <div>
-      <div className="nav-container">
+      {/* <div className="nav-container">
         <nav>
           <Link to="/"> Homepage </Link>
           <Link to="counter"> Your personal Counter </Link>
           <Link to="users/:username"> Users Profile</Link>
         </nav>
-      </div>
+  </div>*/}
       <div className="routes-container">
         <Routes>
-          <Route path="/" element={<Welcome name="Guys" />} />
-          <Route path="counter" element={<ClickCounter />} />
+          <Route path="/" element={<FirstPage />} />
+          <Route path="/:username" element={<Welcome />} />
+          <Route path="/counter/:username" element={<ClickCounter />} />
           <Route path="/users/:username" element={<ShowGithubUser />} />
           <Route path="*" element={<div><h3>Page not found. Sorry! 😞</h3>
               <Link to="/"> Go back to Homepage </Link></div>}
--- Exercise_64/react-router_05/src/Welcome.js
+++ Exercise_64/react-router_05/src/Welcome.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export function Welcome(props) {
+  const {username} = useParams();
   return (
     <div className="welcome">
-      <h1>Welcome {props.name}! 😃</h1>
-      <Link to="counter">Start the Counter now ⏳ </Link>  or   
-      <Link to="users/:username"> Find out your userProfile </Link>
+      <h1>Welcome {username}! 😃</h1>
+      <Link to={`/counter/${username}`}>Start the Counter now ⏳ </Link> or
+      <Link to={`/users/${username}`}> Find out your userProfile </Link>
     </div>
   );
 }
