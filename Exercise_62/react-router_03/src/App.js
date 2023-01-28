c2a5e6ca1be96bc6e748b4d1a5d7bb23f789e3a9
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-28 08:20:48 +0000

    adjustments to Links

--- Exercise_62/react-router_03/src/App.js
+++ Exercise_62/react-router_03/src/App.js
@@ -10,7 +10,7 @@ export function App() {
       <Routes>
         <Route path="/" element={<Welcome name="Guys" />} />
         <Route path="counter" element={<ClickCounter />} />
-        <Route path="/users/:username" element={<ShowGithubUser />} />
+        <Route path="users/:username" element={<ShowGithubUser />} />
       </Routes>
     </div>
   );
