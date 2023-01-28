33f391a4666e544541548d180a72aa37bf94391b
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-28 17:15:28 +0000

    added a first page

--- Exercise_62/react-router_03/src/App.js
+++ Exercise_62/react-router_03/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { ClickCounter } from "./ClickCounter";
 import { Welcome } from "./Welcome";
+import { FirstPage } from "./FirstPage";
 import { ShowGithubUser } from "./ShowGithubUser";
 
 import "./App.css";
@@ -10,6 +11,7 @@ export function App() {
   return (
     <div className="routes-container">
       <Routes>
+        <Route path="/" element={<FirstPage />} />
         <Route path="/:username" element={<Welcome />} />
         <Route path="/counter/:username" element={<ClickCounter />} />
         <Route path="users/:username" element={<ShowGithubUser />} />
---
+++ Exercise_62/react-router_03/src/FirstPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export function FirstPage() {
+  return (
+    <h1>Add an / and your name or nicknameto the path 🔝 and press enter 😏</h1>
+  );
+}
