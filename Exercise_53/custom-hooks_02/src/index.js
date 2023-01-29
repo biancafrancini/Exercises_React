4dbd02e8ff347d01ea177d01ed1316e97d5b6f43
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-29 08:40:38 +0000

    modified README file and adjusted index.js

--- Exercise_53/custom-hooks_02/src/index.js
+++ Exercise_53/custom-hooks_02/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ControlledForm } from './ControlledForm'; 
 
-const form = <ControlledForm />
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(form);
+root.render(<ControlledForm />);
