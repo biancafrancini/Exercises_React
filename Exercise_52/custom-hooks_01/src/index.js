d574194aa8df7a510dda7e8ecd8b0fc0c922e7f9
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-29 08:37:17 +0000

    modified README file and adjusted index.js

--- Exercise_52/custom-hooks_01/src/index.js
+++ Exercise_52/custom-hooks_01/src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {CounterComponent} from './CounterComponent';
 
-const counterComponent = <CounterComponent />
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(counterComponent);
+root.render(<CounterComponent />);
