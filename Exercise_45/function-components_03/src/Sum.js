c6db29e98d543baa5ef34cd6cf114a9530ffcb2f
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-29 09:10:48 +0000

    adjustments

--- Exercise_45/function-components_03/src/Sum.js
+++ Exercise_45/function-components_03/src/Sum.js
@@ -3,23 +3,13 @@ import React from "react";
 export function Sum(props) {
   const { numbers = [1, 2, 3] } = props;
 
-  const getNumbersSum = (array) => {
-    return array.reduce((prev, curr) => prev + curr);
-  };
-
-  return <h1>Total: {getNumbersSum(numbers)} 🫣</h1>;
+  return <h1>Total: {numbers.reduce((prev, curr) => prev + curr)} 🫣</h1>;
 }
-
-/* 
+/*
 or we can set default props in this other way below:
-
 export function Sum(props) {
 
-  const getNumbersSum = (array) => {
-    return array.reduce((prev, curr) => prev + curr);
-  };
-
-  return <h1>Total: {getNumbersSum(props.numbers)} 🫣</h1>;
+return <h1>Total: {props.numbers.reduce((prev, curr) => prev + curr)} 🫣</h1>;
 }
 
 Sum.defaultProps ={
