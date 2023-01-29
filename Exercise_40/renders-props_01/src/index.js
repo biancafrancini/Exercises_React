1d9b9fd87306e3b4d4a48cf3cb8e6df18cdadf75
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-29 08:05:59 +0000

    modified README file and adjusted index.js

--- Exercise_40/renders-props_01/src/index.js
+++ Exercise_40/renders-props_01/src/index.js
@@ -1,7 +1,5 @@
-import ReactDOM from 'react-dom/client';
-import { LogicTodos } from './LogicTodos';
+import ReactDOM from "react-dom/client";
+import { LogicTodos } from "./LogicTodos";
 
-const todosComponent = <LogicTodos />;
-const root = ReactDOM.createRoot(document.querySelector('#root'));
-root.render(todosComponent);
-
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<LogicTodos />);
