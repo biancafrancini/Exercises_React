a94e6cc2b7acb5e79327d5c5be710faf4c7cee2f
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-29 08:43:34 +0000

    modified README file and adjusted index.js

--- Exercise_54/custom-hooks_03/src/index.js
+++ Exercise_54/custom-hooks_03/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { GitHubUser } from "./GitHubUser";
 
-const fetchComponent = <GitHubUser username="Alex"/>
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(fetchComponent);
+root.render(<GitHubUser username="Alex"/>);
