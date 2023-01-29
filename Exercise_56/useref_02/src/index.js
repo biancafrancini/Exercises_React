f33e97b6708fc619242a24fd6c572e89c7185041
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-29 08:48:27 +0000

    modified README file and adjusted index.js

--- Exercise_56/useref_02/src/index.js
+++ Exercise_56/useref_02/src/index.js
@@ -2,6 +2,5 @@ import React from  'react';
 import ReactDOM from 'react-dom/client';
 import CarDetailsForm from './CarDetailsForm';
 
-const CarDetailsFormComponent = <CarDetailsForm />;
 const root = ReactDOM.createRoot(document.querySelector("#root"));
-root.render(CarDetailsFormComponent);
+root.render(<CarDetailsForm />);
