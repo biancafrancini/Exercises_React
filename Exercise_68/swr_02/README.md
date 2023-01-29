8750a669637429683f944d1111575e902c3fb6ba
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-29 18:19:26 +0000

    Initialized Exercise_68, added GitHubUser component, modified custom hook to have conditional data fetching

---
+++ Exercise_68/swr_02/README.md
@@ -0,0 +1,3 @@
+# Exercise_68
+
+Modify the useGithubUser hook so that, if the username is null, no request is made.
---
+++ Exercise_68/swr_02/src/GitHubUser.js
@@ -0,0 +1,83 @@
+import React from "react";
+import useSWR from "swr";
+
+const fetcher = (url) =>
+  fetch(url).then((res) => {
+    if (!res.ok) {
+      throw new Error("Response Error");
+    }
+    return res.json();
+  });
+
+function useGitHubUser(username) {
+  const { data, error } = useSWR(username ? 
+    `https://api.github.com/users/${username}` : null,
+    fetcher,
+  );
+
+  return {
+    users: data,
+    isError: error,
+  }
+}
+
+export function GitHubUser({ username }) {
+  const {users, isError} = useGitHubUser(username)
+
+  return (
+    <div>
+      {!users && !isError && <h3>Loading.... 🫣</h3>}
+      {!users && !isError && !username && <h3>Sorry, there are no data for your request 🧐</h3>}
+      {users && !isError && (
+        <div>
+          <div>
+            <strong>ID</strong>: {users.id}
+          </div>
+          <div>
+            <strong>Username</strong>: {users.login}
+          </div>
+          <div>
+            <strong>Location</strong>:
+            {users.location ? users.location : " unknown"}
+          </div>
+        </div>
+      )}
+      {isError && <h3>There has been an error.. please, try again🥺</h3>}
+    </div>
+  );
+}
+
+
+
+/* Code extracted from Exercise Custom-hooks_04 :
+
+function useGitHubUser(user){
+  const [data, setData] = useState(user);
+
+  async function FetchGitHubUser(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`);
+    const json = await response.json();
+
+    setData(json);
+  }
+
+  useEffect(() => {
+    FetchGitHubUser(user);
+  }, [user]);
+
+  return {
+    _data: data,
+  }
+}
+
+export function GitHubUser({ username }) {
+  const {_data} = useGitHubUser(username)
+
+  return (
+    <div>
+      <div><strong>ID</strong>: {_data.id}</div>
+      <div><strong>Username</strong>: {_data.login}</div>
+      <div><strong>Location</strong>: {_data.location ? _data.location : "unknown"}</div>
+    </div>
+  );
+}*/
