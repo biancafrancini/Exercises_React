5b1fbec42e1d4f1d9b523793159db1604c671849
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-31 14:57:36 +0000

    adjustments

--- Exercise_18/conditional-rendering_04/src/index.js
+++ Exercise_18/conditional-rendering_04/src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
-const $root = document.querySelector("#root");
-
-const helloYou = <App />;
-
-ReactDOM.render(helloYou, $root);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
