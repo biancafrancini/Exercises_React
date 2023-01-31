b5e8de057a52dccab8dce944a8465653a82d953b
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-01-31 14:50:07 +0000

    adjustments

--- Exercise_9/components_07/src/index.js
+++ Exercise_9/components_07/src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
 
-const $root = document.querySelector("#root");
-ReactDOM.render(<App />, $root);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<App />);
