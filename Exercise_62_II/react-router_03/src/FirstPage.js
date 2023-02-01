964ea06602d36782e8f57aaa7458e7857891bce7
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-01 14:33:04 +0000

    made a copy of Exercise_62

---
+++ Exercise_62_II/react-router_03/src/FirstPage.js
@@ -0,0 +1,5 @@
+export function FirstPage() {
+  return (
+    <h1>Add an / and your name or nicknameto the path 🔝 and press enter 😏</h1>
+  );
+}
---
+++ Exercise_62_II/react-router_03/src/Welcome.js
@@ -0,0 +1,12 @@
+import { Link, useParams } from "react-router-dom";
+
+export function Welcome(props) {
+  const {username} = useParams();
+  return (
+    <div className="welcome">
+      <h1>Welcome {username}! 😃</h1>
+      <Link to={`/counter/${username}`}>Start the Counter now ⏳ </Link> or
+      <Link to={`/users/${username}`}> Find out your userProfile </Link>
+    </div>
+  );
+}
