114529051f32784d265d325176b67ed731d56234
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-01 16:15:43 +0000

    Adjustments to match problem request

--- Exercise_65/react-router_06/src/Welcome.js
+++ Exercise_65/react-router_06/src/Welcome.js
@@ -1,12 +1,11 @@
 import { Link, useParams } from "react-router-dom";
 
 export function Welcome(props) {
-  const {username} = useParams();
   return (
     <div className="welcome">
       <h1>Welcome <strong>{props.name}</strong>! 😃</h1>
       <Link to="/counter">Start the Counter now ⏳ </Link> or
-      <Link to={`/users`}> Find out your userProfile </Link>
+      <Link to="/users"> Find out your userProfile </Link>
     </div>
   );
 }
