c611f49fb24ebddae36395f743b9acaa04aba4fb
Bianca_ <86985111+biancafrancini@users.noreply.github.com>
2023-02-01 15:02:35 +0000

    Adjustments to components to match the problem request

--- Exercise_63/react-router_04/src/App.js
+++ Exercise_63/react-router_04/src/App.js
@@ -1,22 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import { ClickCounter } from "./ClickCounter";
 import { Welcome } from "./Welcome";
-import { FirstPage } from "./FirstPage";
 import { ShowGithubUser } from "./ShowGithubUser";
 
 import "./App.css";
 
 export function App() {
   return (
-    <div>
-      <div className="routes-container">
-        <Routes>
-          <Route path="/" element={<FirstPage />} />
-          <Route path="/:username" element={<Welcome />} />
-          <Route path="/counter/:username" element={<ClickCounter />} />
-          <Route path="users/:username" element={<ShowGithubUser />} />
-        </Routes>
-      </div>
+    <div className="routes-container">
+      <Routes>
+        <Route path="/" element={<Welcome name="Alessandro" />} />
+        <Route path="/counter" element={<ClickCounter />} />
+        <Route path="users/:username" element={<ShowGithubUser />} />
+      </Routes>
     </div>
   );
 }
--- Exercise_63/react-router_04/src/ShowGithubUser.js
+++ Exercise_63/react-router_04/src/ShowGithubUser.js
@@ -5,9 +5,12 @@ export function ShowGithubUser() {
   const { username } = useParams();
   return (
     <div>
+      <h4>Insert your name (or nickname) instead of "/undefined" into the path 🔝 and press enter. 
+        <br />
+        See what happens next.. ⤵️ 😏</h4>
       <GitHubUser username={username} />
       <br />
-      <Link to={`/${username}`}> Go back to Homepage</Link>
+      <Link to="/"> Go back to Homepage</Link>
     </div>
   );
 }
